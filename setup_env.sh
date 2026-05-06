10bedc94ef8780f8c7ec612d4607e10e64882705
Shiduo-zh <1372286707@qq.com>
2026-05-06 14:56:34 +0800

    add scripts

---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export OPENPI_DATA_HOME=""
+export HF_LEROBOT_HOME=""
+export HF_HOME=""
+
+echo "Environment variables set:"
+echo "OPENPI_DATA_HOME: $OPENPI_DATA_HOME"
+echo "HF_LEROBOT_HOME: $HF_LEROBOT_HOME"
+echo "HF_HOME: $HF_HOME"
